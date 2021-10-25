a8a4d17db79412a48ef5477a6e224ce6909e3e6a
amarchenko161 <amarchenko.exceedteam@gmail.com>
2021-10-25 16:23:01 +0300

    add routes for server

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const Schema = mongoose.Schema;
 
 const taskScheme = new Schema({
   text: String,
-  isCheck: Boolean
+  isCheck: Boolean,
 });
 
-const uri = 'mongodb+srv://todoDB:restart987@cluster0.lnnws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-const Task = mongoose.model("tasks", taskScheme); 
+app.use(cors());
 
-app.get('/', (req, res) => {
-  const task = new Task({
-    text: 'Second task',
-    isCheck: false
-  });
-  task.save().then(result => {
-    res.send(result);
+const Task = mongoose.model("tasks", taskScheme);
+const uri =
+  "mongodb+srv://todoDB:restart987@cluster0.lnnws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+
+app.get("/allTasks", (req, res) => {
+  Task.find().then((result) => {
+    res.send({ data: result });
   });
 });
 
-app.get('/paramRequest', (req,res) => {
-  Task.find().then(result => {
-    res.send({data: result});
-  })
+app.post("/createTask", (req, res) => {
+  const task = new Task(req.body);
+  task.save().then((result) => {
+    res.send("Task created");
+  });
+});
+
+app.delete("/deleteTask", (req, res) => {
+  Task.deleteOne({ _id: req.query._id }).then((result) => {
+    Task.find().then((result) => {
+      res.send({ data: result });
+    });
+  });
+});
+
+app.patch("/updateTask", (req, res) => {
+  Task.updateOne({ _id: req.query._id }, req.body).then((result) => {
+    Task.find().then((result) => {
+      res.send({ data: result });
+    });
+  });
 });
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log("Example app listening on port 8000!");
 });
-
